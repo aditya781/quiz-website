0b4c9034df0cf91291417be9d9ed4d90dad6bbfe
aditya sonawane <adityasonawane2222@gmail.com>
2021-09-29 17:37:44 +1100

    created backend for register page(on basis of unique email, as a ID)

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express=require("express");
+const ejs=require("ejs")
 const mysql=require("mysql");
+const bcrypt=require("bcrypt")
 const bodyParser=require("body-parser");
 const { response } = require("express");
 require('dotenv').config();
@@ -8,6 +10,10 @@ const app=express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
+const saltRounds=10;
+
+var regi_error=" ";
+var error=" ";
 
 //getting password from .env file
 let mysqlPass=process.env.MYSQL_PASSWORD;
@@ -31,18 +37,49 @@ db.connect((err)=>{
 
 
 app.get("/",function(req,res){  
-    res.render("login.ejs");
+    res.render("login.ejs", {error:" "});
 });
 
 app.get("/login",function(req,res){  
-    res.render("login.ejs");
+    res.render("login.ejs", {error:""});
 });
 app.get("/register",function(req,res){  
-    res.render("register.ejs");
+    res.render("register.ejs", {regi_error:""});
 });
+
+app.post("/login", function(req,err){
+    res.render("login", {error:"welcome aditya"})
+})
 
 app.post("/register",function(req,res){ 
     console.log(req.body)
+    bcrypt.hash(req.body.fpassword, saltRounds, function(err, hash){
+        console.log(hash.length)
+        db.query("select admin_email from admin_table where admin_email= ? ", [req.body.femail], (error, result)=>{
+            if(error){
+                console.log(error);
+                return;
+            }
+            if(result.length>0){
+                console.log("User already exists");
+                res.render("register.ejs", {regi_error:"User already exists!"});
+            }else{
+                db.query("insert into admin_table  set ? ", {
+                    admin_name:req.body.fname, 
+                    admin_pass:hash, 
+                    admin_phone:req.body.fphone,
+                    admin_email:req.body.femail, 
+                    admin_profession:req.body.fprofession}, (error, result)=>{
+                    if(error){
+                        console.log(error);
+                    }
+                })
+
+                res.render("home.ejs");
+            }
+        })
+            
+    });
    
 });
 
--- public/js/login.js
+++ public/js/login.js
@@ -2,8 +2,8 @@
 create table admin_table(
     admin_name varchar(50) not null,
     admin_email varchar(50) not null,
-    admin_pass varchar(50 ) not null,
-    admin_phone int not null,
+    admin_pass varchar(100 ) not null,
+    admin_phone bigint not null,
     admin_profession varchar(50)
 );
 */
